b70176a07816a052a7fd55117cf26f7145d0b6a4
Артем Кунченко <kunchenko911@ukr.net>
2023-09-06 00:25:36 +0300

    Implemented the fabric method

--- Toys (Factory method)/Source.cpp
+++ Toys (Factory method)/Source.cpp
@@ -1,9 +1,54 @@
 #include<iostream>
 using namespace std;
 
+class InjectionMold 
+{
+public:
+    virtual void Toy() = 0;
+};
+
+
+class ToyDuckMold : public InjectionMold 
+{
+public:
+    void Toy() override {   cout << "Toy \"Duck\" is created." << endl; }
+};
+
+
+class ToyCarMold : public InjectionMold 
+{
+public:
+    void Toy() override {cout << "Toy \"Car\" is created." << endl;}
+};
+
+class Manufacturer 
+{
+public:
+    virtual InjectionMold* createToy() = 0;
+};
+
+class ToyDuckManufacturer : public Manufacturer 
+{
+public:
+    InjectionMold* createToy() override {return new ToyDuckMold();}
+};
+
+class ToyCarManufacturer : public Manufacturer 
+{
+public:
+    InjectionMold* createToy() override { return new ToyCarMold();}
+};
+
 
 int main()
 {
+    Manufacturer* toyDuckManufacturer = new ToyDuckManufacturer();
+    Manufacturer* toyCarManufacturer = new ToyCarManufacturer();
+    InjectionMold* toyDuckMold = toyDuckManufacturer->createToy();
+    InjectionMold* toyCarMold = toyCarManufacturer->createToy();
+
+    toyDuckMold->Toy();
+    toyCarMold->Toy();
 
 	system("pause");
 	return 0;
